e04e9dcc078e03c28be7a566f8b48619861bf7ae
peymanzarchini <peymanzarchini@outlook.com>
2023-12-04 20:04:57 +0330

    Create Topbar functionality and modifying some codes

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,5 +1,35 @@
+import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+
 const Topbar = () => {
-  return <h1>Topbar</h1>;
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.png" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Social Media</p>
+      </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default Topbar;
